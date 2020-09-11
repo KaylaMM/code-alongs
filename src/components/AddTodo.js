8905969f256bbec9e6930697e0e1c93122f9a320
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-09-10 23:17:56 -0400

    First commit using kanban board

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,40 +1,38 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-export class AddTodo extends Component {
-  state = {
-    title: "",
+const AddTodo = ({ addTodo }) => {
+  const [todo, setTodo] = useState("");
+
+  const createTodo = (event) => {
+    event.preventDefault();
+    addTodo(todo);
+    setTodo("");
   };
 
-  onSubmit = (e) => {
-    e.preventDefault();
-    this.props.addTodo(this.state.title);
-    this.setState({ title: "" });
+  const onChange = (event) => {
+    setTodo(event.target.value);
   };
 
-  onChange = (e) => this.setState({ title: e.target.value });
-
-  render() {
-    return (
-      <form onSubmit={this.onSubmit} style={{ display: "flex" }}>
-        <input
-          type="text"
-          name="title"
-          style={{ flex: "10", padding: "5px" }}
-          placeholder="Add Todo..."
-          value={this.state.title}
-          onChange={this.onChange}
-        />
-        <input
-          type="submit"
-          value="Submit"
-          className="btn"
-          style={{ flex: "1" }}
-        />
-      </form>
-    );
-  }
-}
+  return (
+    <form onSubmit={createTodo} style={{ display: "flex" }}>
+      <input
+        type="text"
+        name="todo"
+        style={{ flex: "10", padding: "5px" }}
+        placeholder="Add Todo..."
+        value={todo}
+        onChange={onChange}
+      />
+      <input
+        type="submit"
+        value="Submit"
+        className="btn"
+        style={{ flex: "1" }}
+      />
+    </form>
+  );
+};
 
 //Prop Types
 AddTodo.propTypes = {
